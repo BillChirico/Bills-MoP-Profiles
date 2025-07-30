af45686a330fd1a1fb2a7f0a9f6702e7a6c31237
Bill Chirico <bill@chirico.dev>
2025-07-29 23:16:09 -0400

    feat(Golden Lotus): Update NPC locations for quests 4-5
    This commit updates the NPC pickup and turn-in coordinates for the quests 'Laosy Scouting' and 'Given a Second Chance' to new locations, ensuring accurate interactions within the Golden Lotus daily quest chain. The changes maintain consistency across all quest profiles.

--- src/Golden Lotus/Golden_Lotus_01_The_Eternal_Vigil.lua
+++ src/Golden Lotus/Golden_Lotus_01_The_Eternal_Vigil.lua
@@ -1,14 +1,14 @@
 -- Quest Information
 BANETO_DefineProfileName("Golden_Lotus_01_The_Eternal_Vigil")
 BANETO_DefineProfileType("Questing")
-BANETO_SetToSkipPulseQuest()
+-- BANETO_SetToSkipPulseQuest()
 BANETO_SetToSkipTurnInQuest()
 BANETO_DefineQuestStepType([[KillAndLoot]])
 BANETO_DefineQuestId(30307)
 
 -- NPC Information
-BANETO_DefineQuestPickupNPC(-600.13208007812, -4186.1938476562, 41.089248657227, 58408)
-BANETO_DefineQuestTurninNPC(-600.13208007812, -4186.1938476562, 41.089248657227, 58408)
+BANETO_DefineQuestPickupNPC(1215.7375488281, 1047.44921875, 425.9674987793, 58408)
+BANETO_DefineQuestTurninNPC(1215.7375488281, 1047.44921875, 425.9674987793, 58408)
 
 -- Quest Objective
 BANETO_DefineQuestTargetId(65810)
