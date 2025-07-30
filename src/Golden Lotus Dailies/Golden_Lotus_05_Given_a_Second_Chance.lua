b33fd53c1b2dfb7bb1120464541c962d9b7a492e
Bill Chirico <bill@chirico.dev>
2025-07-30 18:16:29 -0400

    chore: Update .gitignore and comment out unused code in Golden Lotus quest script
    This commit adds a custom ignore rule for the 'Samples/' directory in the .gitignore file. Additionally, it comments out unused variables and function definitions in the Golden_Lotus_05_Given_a_Second_Chance.lua script to improve code clarity and maintainability without altering functionality.

--- src/Golden Lotus Dailies/Golden_Lotus_05_Given_a_Second_Chance.lua
+++ src/Golden Lotus Dailies/Golden_Lotus_05_Given_a_Second_Chance.lua
@@ -16,53 +16,52 @@ BANETO_DefineQuestTargetId(84242)
 BANETO_DefineQuestTargetId(59183)
 
 -- Quest Locations
-BANETO_DefineCenter(1443.3646240234, 1143.2777099609, 429.45077514648, 130)
-BANETO_DefineCenter(1487.8192138672, 1098.7009277344, 431.32638549805, 130)
+BANETO_DefineCenter(1443.3646240234, 1143.2777099609, 429.45077514648, 200)
 
 -- Next Quest - Chain to consolidated turn-in handler after completing all quest objectives
 BANETO_SetNextLocalQuestProfile([[Golden_Lotus_TurnIn_All]])
 
 -- Quest Pulse
-_G.BANETO_ExecuteCustomQuestPulse_Questmaster = true
+-- _G.BANETO_ExecuteCustomQuestPulse_Questmaster = true
 -- _G.BANETO_ExecuteCustomQuestPulse_SkipNormalBehavior = true
-local lastStepProgress = nil
-local questID = 30312
-local stepIndex = 1
+-- local lastStepProgress = nil
+-- local questID = 30312
+-- local stepIndex = 1
 
-function _G.BANETO_ExecuteCustomQuestPulse()
-	local questData = C_QuestLog.GetQuestObjectives(questID)
-	local finished = questData[stepIndex]["finished"]
-	local stepProgress = questData[stepIndex]["numFulfilled"]
+-- function _G.BANETO_ExecuteCustomQuestPulse()
+-- 	local questData = C_QuestLog.GetQuestObjectives(questID)
+-- 	local finished = questData[stepIndex]["finished"]
+-- 	local stepProgress = questData[stepIndex]["numFulfilled"]
 
-	-- Check if we have stored progress from the last pulse, if not, initialize it
-	if lastStepProgress == nil then
-		lastStepProgress = stepProgress
-	end
+-- 	-- Check if we have stored progress from the last pulse, if not, initialize it
+-- 	if lastStepProgress == nil then
+-- 		lastStepProgress = stepProgress
+-- 	end
 
-	-- If progress has increased by 1, add the current mob to the blacklist
-	if stepProgress > lastStepProgress and stepProgress == lastStepProgress + 1 then
-		local currentTarget = BANETO_Object("target")
+-- 	-- If progress has increased by 1, add the current mob to the blacklist
+-- 	if stepProgress > lastStepProgress and stepProgress == lastStepProgress + 1 then
+-- 		local currentTarget = BANETO_Object("target")
 
-		-- Check if target is already blacklisted (already healed)
-		if BANETO_IsGuidContainedInGuidBlacklist(currentTarget) then
-			BANETO_Print("Wounded Defender already blacklisted (healed) - Clearing target!")
-			BANETO_ClearTarget()
-			return
-		end
+-- 		-- Check if target is already blacklisted (already healed)
+-- 		if BANETO_IsGuidContainedInGuidBlacklist(currentTarget) then
+-- 			BANETO_Print("Wounded Defender already blacklisted (healed) - Clearing target!")
+-- 			BANETO_ClearTarget()
+-- 			return
+-- 		end
 
-		if currentTarget then
-			BANETO_AddMobToGuidBlacklist("target")
-			BANETO_AddMobToGuidBlacklist(currentTarget)
-			BANETO_AddMobToGuidBlacklist(UnitGUID("target"))
+-- 		if currentTarget then
+-- 			BANETO_AddMobToGuidBlacklist("target")
+-- 			BANETO_AddMobToGuidBlacklist(currentTarget)
+-- 			BANETO_AddMobToGuidBlacklist(UnitGUID("target"))
 
-			BANETO_Print("Healed Wounded Defender - Adding to blacklist!")
-		end
-	end
+-- 			BANETO_Print("Healed Wounded Defender - Adding to blacklist!")
+-- 		end
+-- 	end
 
-	-- Update the stored progress for the next pulse
-	lastStepProgress = stepProgress
+-- 	-- Update the stored progress for the next pulse
+-- 	lastStepProgress = stepProgress
 
-	if finished then
-		BANETO_LoadProfile([[Golden_Lotus_TurnIn_All]])
-	end
-end
+-- 	if finished then
+-- 		BANETO_LoadProfile([[Golden_Lotus_TurnIn_All]])
+-- 	end
+-- end
