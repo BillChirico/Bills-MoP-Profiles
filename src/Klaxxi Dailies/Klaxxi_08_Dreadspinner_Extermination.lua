529291d1f1f9fbda06f7a07cd65ddf432e8cb594
Bill Chirico <bill@chirico.dev>
2025-08-08 15:09:30 -0400

    feat(Klaxxi Dailies): Add new quests "An Ancient Empire" and "Dreadspinner Extermination" with custom quest pulse logic
    This commit introduces two new quest profiles for the Klaxxi Dailies: "Klaxxi_07_An_Ancient_Empire" and "Klaxxi_08_Dreadspinner_Extermination." Each quest includes detailed NPC interactions, objectives, and locations. Additionally, a custom quest pulse function is implemented to manage quest transitions effectively, ensuring a smooth progression between quests. The `.vscode/settings.json` file has also been updated to include new terms for improved spell checking.

---
+++ src/Klaxxi Dailies/Klaxxi_08_Dreadspinner_Extermination.lua
@@ -0,0 +1,35 @@
+-- Quest Information
+BANETO_DefineProfileName("Klaxxi_08_Dreadspinner_Extermination")
+BANETO_DefineProfileType("Questing")
+BANETO_SetToSkipTurnInQuest()
+BANETO_DefineQuestStepType([[KillAndLoot]])
+BANETO_DefineQuestId(31231)
+
+-- NPC Information
+BANETO_DefineQuestPickupNPC(136.87948608398, 3193.1166992188, 214.7607421875, 62538) -- Kil'ruk the Wind-Reaver
+BANETO_DefineQuestTurninNPC(136.87948608398, 3193.1166992188, 214.7607421875, 62538) -- Kil'ruk the Wind-Reaver
+
+-- Quest Objective
+BANETO_DefineQuestTargetId(61981) -- Dreadspinner Tender
+
+-- Quest Locations
+BANETO_DefineCenter(322.75918579102, 2384.0385742188, 202.71076965332, 300)
+BANETO_DefineCenter(360.16882324219, 2507.5568847656, 218.33654785156, 300)
+BANETO_DefineCenter(406.19610595703, 2618.5681152344, 218.66253662109, 300)
+
+-- Next Quest
+BANETO_SetNextLocalQuestProfile([[Klaxxi_TurnIn_All]])
+
+-- Quest Pulse
+BANETO_ExecuteCustomQuestPulse_Questmaster = true
+
+function _G.BANETO_ExecuteCustomQuestPulse()
+    if not BANETO_HasQuest(31231) then
+        BANETO_Print("Quest not found, skipping!")
+
+        BANETO_LoadQuestProfile([[Klaxxi_TurnIn_All]])
+        return
+    else
+        BANETO_ExecuteCustomQuestPulse_Questmaster = false
+    end
+end
